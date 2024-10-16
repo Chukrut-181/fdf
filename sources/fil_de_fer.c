504bfab840891956570ce4122b5dce98c109e9c4
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2024-10-16 16:44:52 +0200

    More Norme.

--- sources/fil_de_fer.c
+++ sources/fil_de_fer.c
@@ -6,7 +6,7 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 16:22:08 by igchurru          #+#    #+#             */
-/*   Updated: 2024/10/16 15:59:57 by igchurru         ###   ########.fr       */
+/*   Updated: 2024/10/16 16:44:40 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	fil_de_fer(t_dot **matrix, t_map *map)
 	mlx_t			*mlx;
 	mlx_image_t		*img;
 
-	mlx = mlx_init(1600, 1200, "test window", true);
+	mlx = mlx_init(1600, 1200, "Fil de Fer", true);
 	img = mlx_new_image(mlx, 1600, 1200);
 	render_matrix(img, matrix, map);
 	mlx_image_to_window(mlx, img, 0, 0);
